c6b9e75615cf0af3dba29788b4d5d2a6d4e770a2
Jacques-rs <gerardrossouw@gmail.com>
2022-06-20 22:30:35 +0200

    progress

--- code/regression_format.R
+++ code/regression_format.R
@@ -20,8 +20,8 @@ regression_format_country_alt <- function(df){
     df <- df %>%
         mutate(day_of_week = lubridate::wday(date, label = T, week_start = 1)) %>%
         mutate(death_rate = new_deaths/new_cases) %>%
-        replace(is.na(.), 0) %>%
-        mutate(year = year(date))
+        replace(is.na(.), 0) #%>%
+        # mutate(year = year(date))
 
 
     return(df)
@@ -41,25 +41,46 @@ regression_format_country_alt <- function(df){
 #
 # }
 
-experiment <- function(df){
+experiment_aggregate <- function(df){
 
 
     df <- df %>%
-        select(-c("death_rate")) %>%
+        # select(-c("death_rate")) %>%
         # need to create a col that indicates which quarter of which year it is
         mutate(year_quarter = paste(year(date), quarter(date), sep = "-")) %>%
         # Then I need to groupby this new Quarter_Year variable to aggreagte the
         # df by this metrics
-        group_by(location, year_quarter)
-        mutate(across(grep(names(.), pattern = "new.*"), sum),
-               across(c("stringency_index", "excess_mortality"), mean)) %>%
-        # group_by(location, year) %>%
+        group_by(location, year_quarter) %>%
         # Calculate the average values for those metrics that we need average values for
-        mutate() %>%
-        # sum those columns up whose values need to be summed (probably none)
-        aggregate(FUN = ) %>%
-        # Will only need the last values of those columns that are set up as
-        # cumulative totals
+        mutate(across(names(df[, grepl(x = names(df), pattern = "new.+")]), sum),
+               across(c("stringency_index", "excess_mortality"), mean)) %>%
+
+
+        return(df)
+}
+
+
+experiment_trim <- function(df){
+
+    df <- df %>%
+        mutate(year_quarter = lubridate::yq(year_quarter)) %>%
+        group_by(location, year_quarter) %>%
+        # filter(last(year_quarter)) %>%
+        slice(tail(row_number(), 1)) %>%
+        select(-c(year_quarter, death_rate)) %>%
+        mutate(death_rate = new_deaths/new_cases)
+
 
 
 }
+
+# experiment_scale <- function(df){
+#
+#
+#
+# }
+
+
+
+
+
